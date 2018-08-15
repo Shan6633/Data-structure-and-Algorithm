4b6691aa13018a9bf03100a707c6b804586ea997
liushan <liushan@wustl.edu>
2018-08-15 18:43:16 -0500

    combination

---
+++ cmake-build-debug/CMakeFiles/Data-structure-and0Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data-structure-and0Algorithm.dir/maze.cpp.o"
+  "CMakeFiles/Data-structure-and0Algorithm.dir/permutation.cpp.o"
+  "CMakeFiles/Data-structure-and0Algorithm.dir/cpmbination.cpp.o"
+  "Data-structure-and0Algorithm.pdb"
+  "Data-structure-and0Algorithm"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Data-structure-and0Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
