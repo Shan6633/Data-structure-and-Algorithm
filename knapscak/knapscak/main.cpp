09121e461a6cdd72883c9395ae5492034d9f4688
liushan <liushan@wustl.edu>
2018-08-15 19:22:20 -0500

    knapsack

---
+++ knapscak/knapscak/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  knapscak
+//
+//  Created by liushan on 8/15/18.
+//  Copyright © 2018 liushan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
